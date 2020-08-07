54ba2119a87ed97bb2ab7000ba6a3e9c04e15075
nsheikh23 <nabi.sheikh23@gmail.com>
2020-08-06 18:39:23 -0700

    Basic analysis done

---
+++ config.py
@@ -0,0 +1,16 @@
+documentation = 'https://finnhub.io/docs/api#company-basic-financials'
+Api_key = 'bs9kujvrh5rahoaohmfg'
+dow = ['MSFT','AAPL','V','JPM','JNJ','WMT','PG','INTC','UNH','XOM','HD','DIS','KO','VZ','MRK',
+      'PFE','CVX','CSCO','BA','MCD','NKE','IBM','UTX','AXP','MMM','GS','CAT','WBA','TRV']
+sp500 = ['MSFT','AAPL','AMZN','FB','BBRK.B','GOOGL','GOOG','JNJ','JPM','V','PG','MA','INTC','UNH',
+        'BAC','T','HD','XOM','DIS','VZ','KO','MRK','CMCSA','CVX','PEP','PFE']
+nasdaq = ['MSFT','AAPL','AMZN','GOOGL','GOOG', 'FB','INTC','CSCO','CMCSA','PEP','ADBE','NVDA','NFLX',
+         'PYPL','COST','AMGN','AVGO','TXN','CHTR','SBUX','QCOM','GILD','MDLZ','FISV','BKNG','INTU',
+         'ADP','CME','TMUS','ISRG','MU']
+state_names = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia",
+               "Delaware", "Florida", "Georgia", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana",
+               "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina",
+               "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon",
+               "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Vermont",
+               "Washington", "Wisconsin", "West Virginia", "Wyoming"]
+
